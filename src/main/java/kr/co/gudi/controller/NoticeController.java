558e1265ae11f45be60a1a2bb6737645d1db5d6b
Yu_so_jin <sp3434@naver.com>
2022-11-07 12:37:21 +0900

    Revert "test소진"
    This reverts commit 6cdccedb52cfa7ab8de4901cef517f98eca82518.

--- src/main/java/kr/co/gudi/controller/NoticeController.java
+++ src/main/java/kr/co/gudi/controller/NoticeController.java
@@ -11,55 +11,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import kr.co.gudi.service.NoticeService;
 import kr.co.gudi.dto.NoticeDTO;
-import kr.co.gudi.dto.PhotoNoticeDTO;
 import kr.co.gudi.service.NoticeService;
 
 @Controller
-public class NoticeController {
-   
-    Logger logger = LoggerFactory.getLogger(this.getClass());
-   
-    @Autowired NoticeService service;
-    
-	
-	@RequestMapping(value = "/notice") public String main() {
-	  
-	 return "notice_list"; 
-	 }
- 
-   
-   @RequestMapping(value = "/notice_list")
-   @ResponseBody
-   public HashMap<String, Object> list(@RequestParam int page) {
-      logger.info("list요청!! : "+page);
-      HashMap<String, Object> map = new HashMap<String, Object>();
-    	  
-      return service.list(page);
-   }
-   
-   
-   
-   
-   
-   
-   
-   
-   
-}
-
-
-
-
-
-
-
-
-/*@Controller
 public class NoticeController {
 	
 	Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -78,32 +35,12 @@ public class NoticeController {
 		return "notice_list";
 	}
 	
-	 @RequestMapping(value="/notice_writeForm") public String writeForm(Model
-			  model) {
-			  return "notice_writeForm"; }
-			  
-	@RequestMapping(value="/notice_write") public String write(Model
-			  model, @RequestParam HashMap<String, String> params, MultipartFile photo) {
-			  logger.info("params : {}",params); 
-			  return service.write(photo, params); 
-			  
-	}
-	
-	@RequestMapping(value="/notice_detail") public String detail(Model
-			  model, @RequestParam String idx) { logger.info("idx = "+idx);
-			  service.detail(idx,model,"detail"); 
-			  
-			  return "detail"; }
-			  
-			 } 
-			 */
-
-
 	
 	
 	
 	
-
+	
+}
 
 
 
